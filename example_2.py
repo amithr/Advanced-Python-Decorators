f6fab6f19b3d292d6091a46cd3cf2d07542031d0
ravindaa <ravindaa@oregonstate.edu>
2022-11-16 21:35:18 +0100

    First commit

---
+++ example_2.py
@@ -0,0 +1,16 @@
+def do_twice(func):
+    def wrapper_do_twice():
+        func()
+        func()
+    return wrapper_do_twice
+
+def do_twice(func):
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
+    return wrapper_do_twice
+
+@do_twice
+def return_greeting(name):
+    print("Creating greeting")
+    return f"Hi {name}"
